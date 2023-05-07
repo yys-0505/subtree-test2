788e4015e0388d812a81b46abc783347457aced7
yys-0505 <yinyishuai@126.com>
2023-05-07 18:09:27 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './subtree/components/header.vue'
 </script>
 
 <template>
+  <Header />
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
